7c1c0228a84186ffbb1a2c21aef9f01fecfb4c84
Jesse Aitken <jaitken@hawkmail.hccfl.edu>
2021-02-23 00:14:57 -0500

    First commit, GUI Created

---
+++ src/main/java/com/beam/App.java
@@ -0,0 +1,50 @@
+package com.beam;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JButton searchButton;
+    private JList list1;
+    private JButton exitButton;
+
+    public App() {
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+
+    public static void main( String[] args )
+    {
+
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().tabbedPane1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize ( 800, 601 );
+        frame.setLocationRelativeTo ( null );
+        frame.setVisible(true);
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
