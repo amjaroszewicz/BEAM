ede5fd6a8ee47e27e7a7a96057b2f8e12560f009
Jesse Aitken <jaitken@hawkmail.hccfl.edu>
2021-03-20 23:25:40 -0400

    Added the functionality to add file name, and path to maintenance window.

--- src/main/java/com/beam/Maintenance.java
+++ src/main/java/com/beam/Maintenance.java
@@ -11,6 +11,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 
 public class Maintenance extends JFrame {
@@ -22,12 +24,14 @@ public class Maintenance extends JFrame {
     public JButton mainMenuButton;
 
 
+
     public static void main(String[] args) {
     }
 
     //constructor
     public Maintenance(){
         //final JFrame frame = new JFrame("App");
+
         this.setTitle("Maintenance");
         this.setContentPane(this.panel1);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -35,6 +39,19 @@ public class Maintenance extends JFrame {
         this.setSize(600, 401);
         this.setLocationRelativeTo(null);
         this.setVisible(false);
+        table1.setVisible(true);
+        table1.setFillsViewportHeight(true);
+        table1.setAutoCreateRowSorter(true);
+        final DefaultTableModel dtm=new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                //all cells false
+                return false;
+            }
+        };
+        dtm.addColumn("File Name");
+        dtm.addColumn("File Path");
+        table1.setModel(dtm);
         final JFileChooser fc = new JFileChooser();
         mainMenuButton.addActionListener(new ActionListener() {
             @Override
@@ -55,6 +72,8 @@ public class Maintenance extends JFrame {
                             File file = fc.getSelectedFile();
                             //This is where a real application would open the file.
                             //log.append("Opening: " + file.getName() + "." + newline);
+                            dtm.addRow(new Object[] {file.getName(),file.getAbsolutePath()});
+
                         } else {
                             //log.append("Open command cancelled by user." + newline);
                         }
