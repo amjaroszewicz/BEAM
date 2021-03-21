5a93465b8ee1439d16c124ac5856bb3624572648
Jesse Aitken <jaitken@hawkmail.hccfl.edu>
2021-03-20 22:02:42 -0400

    FINALLY got the Buttons to work correctly and show both windows.

--- src/main/java/com/beam/Maintenance.java
+++ src/main/java/com/beam/Maintenance.java
@@ -1,12 +1,37 @@
 package com.beam;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Maintenance {
-    private JTable table1;
-    private JPanel panel1;
-    private JButton addFileButton;
-    private JButton removeFileButton;
-    private JButton refreshFilesButton;
-    private JButton mainMenuButton;
+public class Maintenance extends JFrame {
+    public JTable table1;
+    public JPanel panel1;
+    public JButton addFileButton;
+    public JButton removeFileButton;
+    public JButton refreshFilesButton;
+    public JButton mainMenuButton;
+
+    public static void main(String[] args) {
+    }
+
+    //constructor
+    public Maintenance(){
+        //final JFrame frame = new JFrame("App");
+        this.setTitle("Maintenance");
+        this.setContentPane(this.panel1);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.pack();
+        this.setSize(600, 401);
+        this.setLocationRelativeTo(null);
+        this.setVisible(false);
+        mainMenuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Maintenance.this.setVisible(false);
+            }
+        });
+
+    }
+
 }
